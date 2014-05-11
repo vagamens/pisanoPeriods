bf955a2d115c638de83ca3d1e11b75350ff2de08
Kevin Clay <kclayengineer@cableone.net>
2014-05-11 06:25:45 -0600

    Optimization
    Changed many of the functions to that they do
    not directly modify the lists. Also, moved
    said lists so that they are not longer globals.
    
    [ticket: None]
    
    Changes committed:
    	modified:   pisanoPeriods.py

--- pisanoPeriods.py
+++ pisanoPeriods.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-import csv
-import sys
-
 listed = dict()
 
 def memoize(f):
@@ -16,24 +13,25 @@ def fib(n):
 		return 1
 	return fib(n-1) + fib(n-2)
 
-fibonacci = []
-
 def listCount(array):
 	count = 0
 	for i in array:
 		count+=1
 	return count
 
-def listFib():
-	sort = sorted(listed.items(), key=lambda x: x[1])
+def listFib(fib, list):
+	sort = sorted(list.items(), key=lambda x: x[1])
+	tempFib = []
 	for i,j in sort:
-		fibonacci.append(j)
-	listCount(fibonacci)
+		tempFib.append(j)
+	return tempFib
 
-def findMods(modNum, array):
+def findMods(modNum, fib):
+	tempArray = []
 	for i in range(2, modNum):
-		for j in fibonacci:
-			array.append((i, (j, j % i)))
+		for j in fib:
+			tempArray.append((i, (j, j % i)))
+	return tempArray
 
 def findLength(array1, array2):
 	array2.append((0, 'N/A'))
@@ -46,10 +44,8 @@ def findLength(array1, array2):
 				tempArray.append(array1[count])
 		if tempArray not in tempArrays:
 			tempArrays.append(tempArray)
-	for arrayCount in range(listCount(tempArrays)):
+	for tempArray in tempArrays:
 		temporary = []
-		tempArray = tempArrays.pop(0)
-		temporary.append(tempArray.pop(0))
 		for count in range(listCount(tempArray)):
 			if tempArray[0][1][1] != 0:
 				temporary.append(tempArray.pop(0))
@@ -59,33 +55,36 @@ def findLength(array1, array2):
 				array2.append((tempArray[0][0], listCount(temporary)))
 				break
 
-moded = []
-numLength = []
-
 def sanityCheck(count, modNum):
 	if (modNum-1) == 7 and count <= 16:
 		return None, 'Not enough information.', 'Please increase the count of fibonacci numbers', 'or decrease the length count number.'
 	else: return (True, 0, 0)
 
+def writeToFile(infile, array):
+	import csv
+	with open(infile, 'wb') as csvfile:
+		file = csv.writer(csvfile)
+		file.writerow(['Modulus Number', 'Series Length'])
+		file.writerow(['0', 'Division by Zero Error'])
+		file.writerows(array[1:])
+	print(array)
+
 def main(count=0, modNum=0):
+	moded, numLength, fibonacci = []
 	fib(count)
-	listFib()
-	findMods(modNum, moded)
+	fibonacci = listFib(fibonacci, listed)
+	moded = findMods(modNum, fibonacci)
 	findLength(moded, numLength)
 	# print moded
-	with open('PisanoPeriods.csv', 'wb') as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow(['Modulus Number', 'Series Length'])
-		writer.writerow(['0', 'Division by Zero Error'])
-		writer.writerows(numLength[1:])
-	print numLength
+	#writeToFile('PisanoPeriods.csv', numLength) # under development
 
 if __name__ == '__main__':
+	import sys
 	count, modNum = int(sys.argv[1]), (int(sys.argv[2])+1)
 	checks = sanityCheck(count, modNum)
 	if checks[0] == True:
 		main(count, modNum)
 	else:
-		print checks[1]
-		print checks[2]
-		print checks[3]
+		print(checks[1])
+		print(checks[2])
+		print(checks[3])
